1b1c4ebae17b4135717982a15d0c964f1028da87
patrickliamhartley <patrickliamhartley@gmail.com>
2016-08-09 14:27:07 -0700

    Implement working room selector and hack button

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,8 +8,8 @@ var dataStore = [];
 var dataPull = function(data) { 
   //zero out data store so it doesnt reprint old messages
   dataStore = [];
-  for (var i = 0; i < 100; i ++) {
-    if (data.results[i].username !== undefined || data.results[i].text !== undefined) {
+  for (var i = 0; i < data.results.length; i ++) {
+    if (data.results[i].username !== undefined && data.results[i].text !== undefined) {
       dataStore.push(data.results[i]);
     }
   }
@@ -48,13 +48,20 @@ app.fetch = function () {
 };
 
 app.clearMessages = function () {
-  $('.message').empty();
+
+  $('#chats').empty();
+  // $('.message').remove();  
+  // $('.username').remove();
+
+  
+
 };
 
 
 app.addMessage = function (message) {
+
   var messageObj = {
-    username: myname,
+    username: name,
     text: message,
     roomname: "lobby"
   };
@@ -75,47 +82,104 @@ app.addFriend = function () {
 };
 
 app.init = function() {
-  //clear page of old messages
+  
+  // clear page of old messages
   this.clearMessages();
 
   this.fetch();
-  var rooms = {};
+  
+  
+
+  for (var j = 15 ; j > 0; j--) {
+    // $('#chats').append('<div class=username>a</div>');
+    // $('#chats').append('<div class=message>b</div>');
+    // $('.username').text(dataStore[j].username + ': ');
+    // $('.message').text(dataStore[j].text);
+
+      // document.createTextNode((dataStore[j].username + ': ' + ))).addClass("message");
+   
+    $('#chats').prepend('<div class=message><h2>' + dataStore[j].username + 
+      '</h2>: ' + dataStore[j].text + '</div>');
+    
+  }
 
   
-  $('select').empty();
-  for (var j = 0; j < 20; j ++) {
-    $('#chats').append('<div class=message><h2>' + dataStore[j].username + 
-      '</h2>: ' + dataStore[j].text + '</div>');
-    rooms[dataStore[j]] = dataStore[j].roomname;
-  }
-
-  for (var key in rooms) {
-    $('select').append('<option value="rooms[key]">' + rooms[key] + '</option>');
-
-  }
-
-
 
 };
 
-$('form').submit( function (event) {
-  app.addMessage($("input").val());
+app.setRooms = function () {
+  // $('#dropdown').empty();
+  var rooms = {};
+  app.fetch();
+  for ( var i = 0; i < dataStore.length; i ++) {
+    rooms[dataStore[i].roomname] = dataStore[i].roomname;
 
-});  
-
-var myname = prompt('What is your name?');
-//call init every 2 seconds to update chat
-$(document).ready(function() {
+  }
  
-   
+  // for (var key in rooms) {
+  //   console.log("appending"+rooms[key]);
+  //   $('select').append('<option value="'+ rooms[key]+ '"">rooms</option>');
+  // }
+  this.popRooms(rooms);
+};
+
+app.popRooms = function(obj) {
+
+  for (var key in obj) {
+
+    if (obj[key] !==undefined) {
+      var string = obj[key].toString();
+    
+      $('#dropdown').append('<option>' + string + '</option>');
+    }  
+  }
+
+};
+
+app.randCol = function() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+
+// var myname = prompt('What is your name?');
+// call init every 2 seconds to update chat
+$(document).ready(function() {
+
+  $('#sendChat').submit( function (e) {
+
+
   
+
+    app.addMessage($('#userchat').val());
+    e.preventDefault();
+  });  
+  $('#hack').on('click', function() {
+    var rdcolor1 = app.randCol();
+    var rdcolor2 = app.randCol();
+    name = "HACKER ALPHA";
+    $('#userchat').val("<style> body {background-color:" + rdcolor1 + 
+      "; color: " + rdcolor2 + "} </style>" + "BYPASSING SECURITY PROTOCOLS");
+
+
+
+  });
+
+  setInterval ( function() {
+    $('#dropdown').empty();
+    app.setRooms();
+  }, 7000);
   setInterval( function() {
     
 
     app.init();
 
 
-  }, 2000);
+  }, 1000);
 });
 
 // debugger;
