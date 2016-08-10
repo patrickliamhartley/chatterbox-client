383efba5c5433b9edf52d4fee4921b5a8cf26953
patrickliamhartley <patrickliamhartley@gmail.com>
2016-08-09 20:06:11 -0700

    Implement working rooms selector

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,6 +4,10 @@
 
 var app = {};
 var dataStore = [];
+var rooms = {};
+var dataPulled = false;
+var newRoomName = "lobby";
+var killswitch = true;
 // var username
 var dataPull = function(data) { 
   //zero out data store so it doesnt reprint old messages
@@ -14,8 +18,6 @@ var dataPull = function(data) {
     }
   }
 };
-
-
 
 app.send = function (message) {
   $.ajax({
@@ -37,6 +39,11 @@ app.fetch = function () {
     type: 'GET',
     success: function (data) {
       dataPull(data);
+      if (dataPulled === false) {
+        app.setRooms();
+        app.popChat();
+        dataPulled = true;
+      }
     }
     // success: function (data) {
     //   console.log(data);
@@ -48,30 +55,26 @@ app.fetch = function () {
 };
 
 app.clearMessages = function () {
-
   $('#chats').empty();
   // $('.message').remove();  
   // $('.username').remove();
-
-  
-
 };
 
 
 app.addMessage = function (message) {
-
   var messageObj = {
     username: name,
     text: message,
-    roomname: "lobby"
+    roomname: newRoomName
   };
 
   this.send(messageObj);
+  killswitch = true;
   // $('#chats').append('<div>' + message + '</div>');
 };
 
 app.addRoom = function (roomName) {
-  $('#roomSelect').append('<div>roomName</div>');
+  $('#dropdown').append('<option>' + roomName + '</option>');
 
 };
 
@@ -82,104 +85,131 @@ app.addFriend = function () {
 };
 
 app.init = function() {
-  
+  console.log('initiating');
   // clear page of old messages
-  this.clearMessages();
+  // app.clearMessages();
+  app.fetch();
+  app.poll();
 
-  this.fetch();
-  
-  
+};
 
-  for (var j = 15 ; j > 0; j--) {
+app.popChat = function () { 
+  app.clearMessages();
+  for (var j = 15; j > 0; j--) {
     // $('#chats').append('<div class=username>a</div>');
     // $('#chats').append('<div class=message>b</div>');
     // $('.username').text(dataStore[j].username + ': ');
     // $('.message').text(dataStore[j].text);
 
       // document.createTextNode((dataStore[j].username + ': ' + ))).addClass("message");
-   
     $('#chats').prepend('<div class=message><h2>' + dataStore[j].username + 
-      '</h2>: ' + dataStore[j].text + '</div>');
-    
+      '</h2>: ' + dataStore[j].text + '</div>'); 
   }
-
-  
-
 };
 
 app.setRooms = function () {
-  // $('#dropdown').empty();
-  var rooms = {};
   app.fetch();
   for ( var i = 0; i < dataStore.length; i ++) {
     rooms[dataStore[i].roomname] = dataStore[i].roomname;
-
   }
- 
   // for (var key in rooms) {
   //   console.log("appending"+rooms[key]);
   //   $('select').append('<option value="'+ rooms[key]+ '"">rooms</option>');
   // }
-  this.popRooms(rooms);
+  app.popRooms(rooms);
 };
 
 app.popRooms = function(obj) {
-
+  app.addRoom("New Room");
   for (var key in obj) {
-
-    if (obj[key] !==undefined) {
+    if (obj[key] !== undefined) {
       var string = obj[key].toString();
-    
       $('#dropdown').append('<option>' + string + '</option>');
     }  
   }
-
 };
 
 app.randCol = function() {
   var letters = '0123456789ABCDEF';
   var color = '#';
   for (var i = 0; i < 6; i++ ) {
-      color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 };
+
+app.poll = function () {
+  setInterval( function() {
+
+
+    if (killswitch === true) {  
+      app.fetch();
+      app.popChat();
+    }
+
+  }, 1000);
+};
+
+app.stop = function() {
+  clearInterval(app.poll);
+};
+
+app.roomChange = function ( num) {
+  if ( num === 0 ) {
+    newRoomName = prompt("What would you like to name your new chatroom?");
+    app.addRoom(newRoomName);
+  }  else {
+    newRoomName = $('#dropdown').val();
+  }
+  $('#chats').empty();
+  killswitch = false;
+  dataStore.forEach(function (element){
+    if ( element.roomname===newRoomName){
+      $('#chats').prepend('<div class=message><h2>' + element.username + 
+      '</h2>: ' + element.text + '</div>');
+    }
+    
+  });
+};
+
 
 
 // var myname = prompt('What is your name?');
 // call init every 2 seconds to update chat
 $(document).ready(function() {
-
+  // event listeners
   $('#sendChat').submit( function (e) {
-
-
-  
-
     app.addMessage($('#userchat').val());
     e.preventDefault();
-  });  
+  }); 
+
+  $('#dropdown').on('change', function(ev) {
+    var menuNum = $('#dropdown').prop('selectedIndex');
+    app.roomChange(menuNum);
+  });
+
+
   $('#hack').on('click', function() {
     var rdcolor1 = app.randCol();
     var rdcolor2 = app.randCol();
-    name = "HACKER ALPHA";
-    $('#userchat').val("<style> body {background-color:" + rdcolor1 + 
-      "; color: " + rdcolor2 + "} </style>" + "BYPASSING SECURITY PROTOCOLS");
-
-
-
+    name = 'HACKER ALPHA';
+    $('#userchat').val('<style> body {background-color:' + rdcolor1 + 
+      '; color: ' + rdcolor2 + '} </style>BYPASSING SECURITY PROTOCOLS');
   });
+  // setInterval ( function() {
+  //   $('#dropdown').empty();
+  //   app.setRooms();
+  // }, 5000);
 
-  setInterval ( function() {
-    $('#dropdown').empty();
-    app.setRooms();
-  }, 7000);
-  setInterval( function() {
+  // setInterval( function() {
     
+  //   app.fetch();
+  //   app.popChat();
 
-    app.init();
+
+  // }, 1000);
 
 
-  }, 1000);
 });
 
 // debugger;
